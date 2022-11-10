2509fe1d179cc2216de608c284eb0303c514f8d6
tuzbtla <384434518@qq.com>
2022-11-10 17:15:54 +0800

    “加入显示天气信息的功能”

---
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -0,0 +1,150 @@
+package com.coolweather.android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.coolweather.android.databinding.ActivityWeatherBinding;
+import com.coolweather.android.gson.Forecast;
+import com.coolweather.android.gson.Weather;
+import com.coolweather.android.util.HttpUtil;
+import com.coolweather.android.util.Utility;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.Random;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    private ActivityWeatherBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        binding = ActivityWeatherBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String bingPic = preferences.getString("bing_pic", null);
+        if (bingPic != null) {
+            Glide.with(this).load(bingPic).into(binding.bingPicImg);
+        } else {
+            loadBingPic();
+        }
+        String weatherString = preferences.getString("weather", null);
+        if (weatherString != null) {
+            Weather weather = Utility.handleWeatherResponse(weatherString);
+            showWeatherInfo(weather);
+        } else {
+            String weatherId = getIntent().getStringExtra("weather_id");
+            binding.weatherLayout.setVisibility(View.INVISIBLE);
+            requestWeather(weatherId);
+        }
+
+    }
+
+    /**
+     * 加载必应每日一图
+     */
+    private void loadBingPic() {
+        Random random = new Random();
+        int index = random.nextInt(9);
+        String requestBingPic = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=" + index + "&n=1";
+        HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                String bingPic = Utility.handleBingPicResponse(responseText);
+                SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                editor.putString("bing_pic", bingPic);
+                editor.apply();
+                runOnUiThread(() -> {
+                    Glide.with(WeatherActivity.this).load(bingPic).into(binding.bingPicImg);
+                });
+            }
+        });
+    }
+
+    /**
+     * 根据天气id请求城市天气信息
+     */
+    private void requestWeather(String weatherId) {
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=666";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(() -> {
+                    Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                final Weather weather = Utility.handleWeatherResponse(responseText);
+                runOnUiThread(() -> {
+                    if (weather != null && "ok".equals(weather.status)) {
+                        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(WeatherActivity.this).edit();
+                        editor.putString("weather", responseText);
+                        editor.apply();
+                        showWeatherInfo(weather);
+                    } else {
+                        Toast.makeText(WeatherActivity.this, "解析天气信息失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        loadBingPic();
+    }
+
+    private void showWeatherInfo(Weather weather) {
+        String cityName = weather.basic.cityName;
+        String updateTime = weather.basic.update.updateTime.split(" ")[1];
+        String degree = weather.now.temperature + "℃";
+        String weatherInfo = weather.now.more.info;
+        binding.title.titleCity.setText(cityName);
+        binding.title.titleUpdateTime.setText(updateTime);
+        binding.now.degreeText.setText(degree);
+        binding.now.weatherInfoText.setText(weatherInfo);
+        binding.forecast.forecastLayout.removeAllViews();
+        for (Forecast forecast : weather.forecastList) {
+            View view = LayoutInflater.from(this).inflate(R.layout.forecast_item, binding.forecast.forecastLayout, false);
+            ((TextView)view.findViewById(R.id.date_text)).setText(forecast.date);
+            ((TextView)view.findViewById(R.id.info_text)).setText(forecast.more.info);
+            ((TextView)view.findViewById(R.id.max_text)).setText(forecast.temperature.max);
+            ((TextView)view.findViewById(R.id.min_text)).setText(forecast.temperature.min);
+            binding.forecast.forecastLayout.addView(view);
+        }
+        if (weather.aqi != null) {
+            binding.aqi.aqiText.setText(weather.aqi.city.aqi);
+            binding.aqi.pm25Text.setText(weather.aqi.city.pm25);
+        }
+        String comfort = "舒适度：" + weather.suggestion.comfort.info;
+        String carWash = "洗车指数：" + weather.suggestion.carWash.info;
+        String sport = "运动建议：" + weather.suggestion.sport.info;
+        binding.suggestion.comfortText.setText(comfort);
+        binding.suggestion.carWashText.setText(carWash);
+        binding.suggestion.sportText.setText(sport);
+        binding.weatherLayout.setVisibility(View.VISIBLE);
+    }
+}
--- app/src/main/java/com/coolweather/android/util/Utility.java
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -1,10 +1,13 @@
 package com.coolweather.android.util;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
+import com.coolweather.android.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -77,5 +80,42 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将返回得到JSON数据解析成图片资源的url
+     */
+    public static String handleBingPicResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONObject boot = new JSONObject(response);
+                JSONArray images = boot.getJSONArray("images");
+                JSONObject jsonObject = images.getJSONObject(0);
+                String url = jsonObject.getString("url");
+                Log.d("TAG1", "handleBingPicResponse: " + url);
+                return ("https://cn.bing.com" + url);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+
+
     }
 }
