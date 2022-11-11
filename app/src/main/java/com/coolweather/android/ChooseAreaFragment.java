e0922e24cb7faca3db42a7d114914fec7fe236fb
tuzbtla <384434518@qq.com>
2022-11-11 19:26:02 +0800

    修复碎片的沉浸式状态栏与系统UI过近

--- app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.coolweather.android;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -96,6 +97,10 @@ public class ChooseAreaFragment extends Fragment {
         listView.setAdapter(adapter);
         progressLayout = view.findViewById(R.id.progress_layout);
         titleLayout = view.findViewById(R.id.title_layout);
+        if (!(getActivity() instanceof WeatherActivity)) {
+            view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getActivity().getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         return view;
     }
 
